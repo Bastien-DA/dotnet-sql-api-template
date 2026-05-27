2681ee849053b6630f95d356512fcffb6ca7541c
Bastien-DA <deamezagabastien@outlook.fr>
2026-05-27 15:26:12 +0200

    feat add cpm

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
     .WithDataVolume()
-    .WithPgAdmin();
+    .WithPgWeb();
 
 var appdb = postgres.AddDatabase("appdb");
 
