ce43bcb6bf12980a91ca282cb8e87c9c459eae9b
Bastien-DA <deamezagabastien@outlook.fr>
2026-05-27 15:30:40 +0200

    feat: add editorconfig and format

--- src/ServiceDefaults/Extensions.cs
+++ src/ServiceDefaults/Extensions.cs
@@ -33,7 +33,7 @@ public static class Extensions
             // Turn on service discovery by default
             http.AddServiceDiscovery();
         });
-        
+
         return builder;
     }
 
